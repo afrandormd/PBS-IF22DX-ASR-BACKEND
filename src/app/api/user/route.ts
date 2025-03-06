853f91ef2c0768830c9e184138eb811686e996a1
afrandormd <afrandormd@gmail.com>
2025-03-06 20:03:38 +0700

    chore: init awal

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,24 +1,22 @@
-//buat variable prisma
-
-import { apiResponse } from "@/utils/response";
 import { PrismaClient } from "@prisma/client";
-import { get } from "http";
 import { NextRequest, NextResponse } from "next/server";
 
-//buat variable prisma
-const prisma = new PrismaClient
+// Initialize Prisma Client
+const prisma = new PrismaClient();
 
-export const GET = async (request: NextRequest) => {
-    try {
-      // Ambil semua data dari tabel tb_user
-      const users = await prisma.tb_user.findMany();
-  
-      // Gunakan apiResponse untuk membentuk response API
-      return apiResponse("Success", 200, "OK", users);
-  
-    } catch (error) {
-      console.error("Error fetching users:", error);
-  
-      return apiResponse("Internal Server Error", 500, "ERROR", null);
-    }
-  };
+// Define the GET function
+export async function GET(request: NextRequest) {
+  const users = await prisma.tb_user.findMany();
+  return NextResponse.json({
+    meta_data: {
+      error: 0,
+      list: "Data User",
+
+    },
+    data_user: users,
+  },
+    {
+      status: 200,
+    },
+  );
+}
