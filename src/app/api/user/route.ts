ef952b32d7522c2dc4b82d0b99f24561aaae467a
afrandormd <afrandormd@gmail.com>
2025-03-10 20:55:47 +0700

    feat: menambahkan file env.example

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -6,17 +6,35 @@ const prisma = new PrismaClient();
 
 // Define the GET function
 export async function GET(request: NextRequest) {
-  const users = await prisma.tb_user.findMany();
-  return NextResponse.json({
-    meta_data: {
-      error: 0,
-      list: "Data User",
+  const users = await prisma.tb_user.findMany({});
+  //jika data tidak ada
+  if (users.length == 0) {
+    return NextResponse.json(
+      {
+        metaData: {
+          error: 1,
+          message: "Data User Tidak Ditemukan",
+          status: 404,
+        },
+      },
+      {
+        status: 404,
+      }
+    );
+  }
 
+  return NextResponse.json(
+    {
+      metaData: {
+        error: 0,
+        message: null,
+        status: 200,
+      },
+
+      data_user: users,
     },
-    data_user: users,
-  },
     {
       status: 200,
-    },
+    }
   );
 }
