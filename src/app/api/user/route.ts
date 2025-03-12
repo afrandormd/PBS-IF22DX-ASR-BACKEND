0fde2d223fda8c40027f2497a8283dca15d3efa7
afrandormd <afrandormd@gmail.com>
2025-03-12 20:19:50 +0700

    feat: update service POST untuk validasi data

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -7,8 +7,8 @@ const prisma = new PrismaClient();
 // Define the GET function
 export async function GET(request: NextRequest) {
   const users = await prisma.tb_user.findMany({});
-  //jika data tidak ada
-  if (users.length == 0) {
+  // jika data tidak ada
+  if (users.length === 0) {
     return NextResponse.json(
       {
         metaData: {
@@ -43,29 +43,51 @@ export async function GET(request: NextRequest) {
 // Buat service "POST" (tb_user)
 export const POST = async (request: NextRequest) => {
   // buat object untuk data isian
-  const { nama_value, username_value, password_value } = await request.json()
+  const { nama_value, username_value, password_value } = await request.json();
+
+  // cek apakah username sudah pernah dibuat atau belum
+  const check = await prisma.tb_user.findMany({
+    where: {
+      username: username_value,
+    },
+  });
+
+  // jika data username ditemukan
+  if (check.length === 1) {
+    return NextResponse.json(
+      {
+        metaData: {
+          error: 1,
+          message: "Data User Gagal Disimpan ! Username telah terdaftar.",
+          status: 409,
+        },
+      },
+      {
+        status: 409,
+      }
+    );
+  }
 
   // simpan datanya
   const save = await prisma.tb_user.create({
     data: {
       nama: nama_value,
       username: username_value,
-      password: password_value
+      password: password_value,
     },
-  })
+  });
 
   // proses atau response API
-  return NextResponse.json({
-    metaData: {
-      error: 0,
-      message: "Data user berhasil disimpan",
+  return NextResponse.json(
+    {
+      metaData: {
+        error: 0,
+        message: "Data user berhasil disimpan",
+        status: 201,
+      },
+    },
+    {
       status: 201,
     }
-  },
-    {
-      status: 201
-    }
   );
-}
-
-
+};
