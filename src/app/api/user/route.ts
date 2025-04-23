a34700a50a40cc9967145a0920877d1db29aed4b
afrandormd <afrandormd@gmail.com>
2025-04-23 20:23:14 +0700

    chore: membuat validasi ketika data user tidak ditemukan

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
   const users = await prisma.tb_user.findMany({});
 
   // jika data tidak ada
-  if (users.length != 0) {
+  if (users.length == 0) {
     // return getResponseUserNotFound
     return NextResponse.json(
   {
